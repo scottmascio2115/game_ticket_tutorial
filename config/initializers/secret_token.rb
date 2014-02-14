0a9c085513ad0037911b05ba33fc5db98940e4e7
Scott Mascio <scottmascio@gmail.com>
2014-02-14 15:11:45 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameTicket::Application.config.secret_key_base = '88ba9def15a4d6dcd6af16c3f966f98c8aff6b7d54649e7d5505e0cd1e189655cb327a5aa4a358ee3ba891db87cab81b2a76ba4f862cb415f939e0ad5f0b4ce2'
