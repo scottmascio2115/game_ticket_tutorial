8de7fe62b82752ff4bf8b169808324f6bc0fad95
Scott Mascio <scottmascio@gmail.com>
2014-02-17 16:14:23 -0600

    Restrict access to project actions to admins only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
+
   def require_signin!
     if current_user.nil?
       flash[:error] = "You need to sign in or sign up before continuing."
@@ -13,10 +14,12 @@ class ApplicationController < ActionController::Base
 
   helper_method :require_signin!
 
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
+
 
 end
